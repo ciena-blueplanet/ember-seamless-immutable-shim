d392370761eacb6c6ff295147d7979bfa6cfb63e
Matthew Dahl <dahl.matthew@gmail.com>
2017-02-14 14:34:48 -0800

    Update index.js

--- blueprints/ember-seamless-immutable-shim/index.js
+++ blueprints/ember-seamless-immutable-shim/index.js
@@ -1,15 +1,10 @@
 module.exports = {
   afterInstall: function () {
-    return this.addPackagesToProject([
-      {name: 'seamless-immutable', target: '6.3.0'}
-    ])
-      .then(() => {
-        return this.addAddonsToProject({
-          packages: [
-            {name: 'ember-get-config', target: '0.2.1'}
-          ]
-        })
-      })
+    return this.addAddonsToProject({
+      packages: [
+        {name: 'ember-get-config', target: '0.2.1'}
+      ]
+    })
   },
 
   normalizeEntityName: function () {
